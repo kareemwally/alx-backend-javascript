fe0c02eb566e724bb04e7dc3a537d2e346224dda
kareem <kareemwally@std.mans.edu.eg>
2024-10-01 03:30:16 +0300

    budget obj

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,10 +4,10 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
- const res = { 
+  const res = {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
     [`capita-${getCurrentYear()}`]: capita,
- };
+  };
   return res;
 }
