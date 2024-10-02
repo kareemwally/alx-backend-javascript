4ab6bef94d75d97e8adada08aabc1298d099dac7
kareem <kareemwally@std.mans.edu.eg>
2024-10-03 01:15:57 +0300

    then, catch

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(Error());
-    }
-  }).then((res) => {
+  return promise.then(() => {
     console.log('Got a response from the API');
-    return res;
-  }).catch((err) => err);
+    return { status: 200, body: 'Success' };
+  })
+    .catch(new Error());
 }
