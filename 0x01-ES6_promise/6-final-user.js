650dfcd7da8135860fdadedbb2bc138dae0f4c66
kareem <kareemwally@std.mans.edu.eg>
2024-10-03 03:21:39 +0300

    status and value of each promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,15 @@
-import signUpUser from './4-user-promise'
-import uploadPhoto from './5-photo-reject'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const user = signUpUser(firstName, lastName);
-  const photo = uploadPhoto(fileName);
-  const promises = [user, photo];
-  const res = [];
-  return Promise.allSettled(promises).then((promises) => {
-    for (let promise in promises) {
-	    res.push({
-              status: promise.status,
-              value: promise.status === 'fulfilled' ? res.value : res.reason,
-	    })
-    }
-  });
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
+
+  return Promise.allSettled(promises)
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
