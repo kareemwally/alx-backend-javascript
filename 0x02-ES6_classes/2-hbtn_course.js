09ff21bc40c1ffbcf11ae78f9ea6347382062310
kareem <kareemwally@std.mans.edu.eg>
2024-10-04 02:59:04 +0300

    hbtn is the best

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,41 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be string');
+    } else {
+      this.name = name;
+    } if (typeof length !== 'number') {
+      throw TypeError('Length must be Number');
+    } else {
+      this.length = length;
+    } if (typeof students !== 'object') {
+      throw TypeError('students must be Array of strings');
+    } else {
+      this.students = students;
+    }
+  }
+
+  set length(length) {
+    this._length = length;
+  }
+
+  set students(students) {
+    this._students = students;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
