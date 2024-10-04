6379b583a5db646e32de91fdfab35f6789b96a57
kareem <kareemwally@std.mans.edu.eg>
2024-10-04 03:55:58 +0300

    price is price

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+import Currency from './3-currency';
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  set amount(val) {
+    if (typeof val !== 'number') {
+      throw TypeError('amount must be number');
+    }
+    this._amount = val;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(val) {
+    if (val instanceof Currency) {
+      this._currency = val;
+    } else {
+      throw TypeError('currency must be instance of Currency class');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code}).`;
+  }
+}
