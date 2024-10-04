223dea6019f9810b70a9069f251c087a3dc6858a
kareem <kareemwally@std.mans.edu.eg>
2024-10-04 04:52:36 +0300

    tostrnig

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,32 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof val !== 'string') {
+      throw TypeError('name must be string');
+    }
+    this._name = val;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(val) {
+    if (typeof val !== 'string') {
+      throw TypeError('code must be string');
+    }
+    this._code = val;
+  }
+
+  tostring() {
+    return this._code;
+  }
+}
