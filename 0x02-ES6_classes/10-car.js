27d0d6c5754eaa64d5cb78f8e16508d43984158d
kareem <kareemwally@std.mans.edu.eg>
2024-10-04 05:29:42 +0300

    car

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,20 +1,23 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
-export default class Car {
+export class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
   }
 
   cloneCar() {
-    const Constructor = this.constructor[Symbol.species] || this.constructor;
-    return new Constructor();
-  }
-
-  static get [Symbol.species]() {
-    return this;
+    return new Car(this._brand, this._motor, this._color);
   }
 }
