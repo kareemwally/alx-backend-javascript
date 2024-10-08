7fafcd1a8cc1eb44b339cd41a7d87a7f8a0fd1e4
kareem <kareemwally@std.mans.edu.eg>
2024-10-08 14:23:43 +0300

    Array

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(l) {
-  if (typeof l !== 'object' || l.length === undefined) {
+  if (!Array.isArray(l)) {
     return [];
   }
   return l.map((ele) => ele.id);
