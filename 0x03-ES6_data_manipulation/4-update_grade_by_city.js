a92a11e49591a69913f957c595ada1d840a7f292
kareem <kareemwally@std.mans.edu.eg>
2024-10-09 00:55:29 +0300

    update by city

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+export default function updateStudentGradeByCity(list, city, newGrades) {
+  return list.filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
+}
