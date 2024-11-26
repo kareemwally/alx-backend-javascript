4f11ec07052898e11e32d50036dc269c2861ae7b
kareem <kareemwally@std.mans.edu.eg>
2024-11-26 20:52:37 +0200

    CSV

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,42 +1,26 @@
 const fs = require('fs');
 
-function countStudents(filePath) {
-  const sheet = [];
+function countStudents(path) {
   try {
-    const data = fs.readFileSync(filePath, 'utf8');
-    const fields = data.split('\n')[0].split(',');
-    const users = data.split('\n').slice(1);
-    const subjects = [];
-    for (let i = 0; i < users.length; i += 1) {
-      const user = users[i].split(',');
-      const obj = {};
-      for (let j = 0; j < fields.length; j += 1) {
-        if (j === fields.length - 1) {
-          obj[fields[j].slice(0, 5)] = user[j].split('\r')[0];
-          if (subjects.indexOf(user[j].split('\r')[0]) === -1)subjects.push(user[j].split('\r')[0]);
-          break;
-        }
-        obj[fields[j]] = user[j];
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const studentLines = lines.slice(1);
+    const totalStudents = studentLines.length;
+    console.log(`Number of students: ${totalStudents}`);
+    const fieldGroups = {};
+    studentLines.forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+      if (!fieldGroups[field]) {
+        fieldGroups[field] = [];
       }
-      sheet.push(obj);
+      fieldGroups[field].push(firstname);
+    });
+    for (const [field, students] of Object.entries(fieldGroups)) {
+      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
     }
-    console.log(`Number of students: ${sheet.length}`);
-    for (let i = 0; i < subjects.length; i += 1) {
-      const obj = {
-        sub: subjects[i],
-        number: 0,
-        name: [],
-      };
-      for (let j = 0; j < sheet.length; j += 1) {
-        if (sheet[j].field === subjects[i]) {
-          obj.number += 1;
-          obj.name.push(sheet[j].firstname);
-        }
-      }
-      console.log(`Number of students in ${obj.sub}: ${obj.number}. List: ${obj.name.join(', ')}`);
-    }
-  } catch (err) {
-    console.error('Cannot load the database');
+  } catch (error) {
+    throw new Error('Cannot load the database');
   }
 }
+
 module.exports = countStudents;
