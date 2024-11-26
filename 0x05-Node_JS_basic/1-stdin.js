b89e40b561760193ab7f772b31b72da5fe720de4
kareem <kareemwally@std.mans.edu.eg>
2024-11-26 16:53:06 +0200

    Your Name

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,8 @@
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (data) => {
+  console.log(`Your name is: ${data}`);
+  process.on('exit', () => {
+    console.log('This important software is now closing');
+  });
+  process.exit();
+});
