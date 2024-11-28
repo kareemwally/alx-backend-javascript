b272da211addaf93f4f86ad240af54e21c1ba3e8
kareem <kareemwally@std.mans.edu.eg>
2024-11-28 03:04:37 +0200

    unittesting

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -21,4 +21,12 @@ describe('calculator', function() {
     it('the result must be -2', function() {
       assert.equal(calculateNumber(-4.3, 2), -2);
     });
+
+    it('the result must be 6', function() {
+      assert.equal(calculateNumber(5.2, 0.5), 6);
+    });
+
+    it('the result must be 0', function() {
+      assert.equal(calculateNumber(0.04, 0.03), 0);
+    });
 });
