94cd543d5887184037a60a723e4dab60cb6a8a98
kareem <kareemwally@std.mans.edu.eg>
2024-11-28 07:36:47 +0200

    using exoect from chai

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,64 @@
+const calculateNumber = require('./2-calcul_chai');
+const expect = Chai.expect;
+
+describe('calculator', function() {
+  describe('SUM', function() {
+    it('the result must be 10', function() {
+      expect(calculateNumber('SUM', 5.6, 3.9)).to.equal(10);
+    });
+
+    it('the result must be -1', function() {
+      expect(calculateNumber('SUM', 5.6, -7.4)).to.equal(-1);
+    });
+
+    it('the result must be 6', function() {
+      expect(calculateNumber('SUM', 5.4, 0.5)).to.equal(6);
+    });
+
+    it('the result must be 0', function() {
+      expect(calculateNumber('SUM', 0, 0)).to.equal(0);
+    });
+  });
+  deacribe('SUBTRACT', function() {
+    it('the result must be 2', function() {
+      expect(calculateNumber('SUBTRACT', 5.6, 3.9)).to.equal(2);
+    });
+
+    it('the result must be 13', function() {
+      expect(calculateNumber('SUBTRACT', 5.6, -7.4)).to.equal(13);
+    });
+
+    it('the result must be -6', function() {
+      expect(calculateNumber('SUBTRACT', -5.4, 0.5)).to.equal(-6);
+    });
+
+    it('the result must be 4', function() {
+      expect(calculateNumber('SUBTRACT', 5.2, 0.5)).to.equal(4);
+    });
+
+    it('the result must be 0', function() {
+      expect(calculateNumber('SUBTRACT', 5.2, 5.2)).to.equal(0);
+    });
+
+    it('the result must be 0', function() {
+      expect(calculateNumber('SUBTRACT', 0, 0)).to.equal(0);
+    });
+  });
+  describe('DIVIDE', function() {
+    it('the result must be 1.5', function() {
+      expect(calculateNumber('DIVIDE', 3, 2)).to.equal(1.5);
+    });
+
+    it('the result must be -1.6666666666666667', function() {
+      expect(calculateNumber('DIVIDE', -5, 2.5)).to.equal(-1.6666666666666667);
+    });
+
+    it('the result must be 0', function() {
+      expect(calculateNumber('DIVIDE', 0, 2)).to.equal(0);
+    });
+
+    it('the result must be Error', function() {
+      expect(calculateNumber('DIVIDE', 0, 0)).to.equal('Error');
+    });
+  });
+});
